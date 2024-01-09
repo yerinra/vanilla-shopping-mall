09bc0407d0e8f603af34ceff366c06c18b0eea27
yerinra <yerinaleph@gmail.com>
2024-01-09 19:25:44 +0900

    ✨ Feat: 에러 페이지 구현

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,1 +1,15 @@
-export default function ErrorPage() {}
+import { routeChange } from "../router.js";
+
+export default function ErrorPage({ $app }) {
+  this.$page = document.createElement("div");
+  this.$page.innerHTML =
+    "<h1>ERROR 404</h1><div>Oops! Wrong Path!</div><button>상품 보러 가기</button>";
+
+  $app.append(this.$page);
+
+  this.$page.addEventListener("click", (e) => {
+    const $button = e.target.closest("button");
+    if (!$button) return;
+    routeChange("/");
+  });
+}
